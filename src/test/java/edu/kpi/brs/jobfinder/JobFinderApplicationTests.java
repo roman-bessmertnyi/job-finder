a94171d4737299567bf0527f92e21d7fd11a0cf2
robe0519 <roman.bessmertnyi@netcracker.com>
2019-11-29 11:07:28 +0200

    Initial commit

---
+++ src/test/java/edu/kpi/brs/jobfinder/JobFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kpi.brs.jobfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
